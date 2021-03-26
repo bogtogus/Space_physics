95c9c683535e8a2f4d64be38a1e1a6ee5cb877a8
bogtogus <75621064+bogtogus@users.noreply.github.com>
2021-03-26 21:29:56 +0300

    Доработки
    Замена 3-х классов на 1, доработки

--- main.py
+++ main.py
@@ -16,32 +16,24 @@ class settings:
         self.size = size
         self.screen = None
 
-class star:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.vx = 0
-        self.vy = 0
-        self.r = 16
-        self.m = 1
 
-class planet:
-    def __init__(self, x, y):
+class obj:
+    def __init__(self, x, y, img, r, m):
         self.x = x
         self.y = y
+        self.r = r
+        self.m = m
+        self.img = img
         self.vx = 0
         self.vy = 0
-        self.r = 10
-        self.m = 1000
+    def draw(self, zoom):
+        display.blit(pygame.transform.scale(self.img, (int(self.img.get_width() * zoom), \
+            int(self.img.get_height() * zoom))), \
+            # x:
+            (int((space_objects[i].x + movement[0] - self.r) * zoom), \
+            # y:
+            int((space_objects[i].y + movement[1] - self.r) * zoom)))
 
-class hole:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.vx = 0
-        self.vy = 0
-        self.r = 30
-        self.m = 1000000
 
 cur_time = int(round(time() * 1000))
 cadrs = 0
@@ -49,17 +41,16 @@ fps = 0
 def fps_val(): # значение кадров/c
     global cur_time, cadrs, fps
     cadrs += 1
-    pygame.draw.rect(user.screen, 'black', (0, 0, 30, 80))
+    #pygame.draw.rect(user.screen, 'black', (0, 0, 30, 80))
     if int(round(time() * 1000)) - cur_time > 1000:
         fps = cadrs
         cadrs = 0
         cur_time = int(round(time() * 1000))
-    user.screen.blit(font.render(str(fps) + ' fps', True, (255, 255, 0)), (0, 0))
-    user.screen.blit(font.render(str(len(space_objects)) + ' obj', True, (230, 230, 230)), (0, 30))
+    data = str(fps) + ' fps, ' + str(len(space_objects)) + ' obj, zoom: ' + str(zoom) + ', ' + '[end]: ' + \
+        str((int((space_objects[-1].x + movement[0]) * zoom), int((space_objects[-1].y + movement[1]) * zoom))) + \
+        ', Real: ' + str((int(space_objects[-1].x), int(space_objects[-1].y)))
+    user.screen.blit(font.render(data, True, (200, 200, 200)), (0, 0))
 
-    user.screen.blit(font.render('[0]: ' + str((int((space_objects[0].x + movement[0]) * zoom), \
-        int((space_objects[0].y + movement[1]) * zoom))) + ', Real: ' + str((int(space_objects[0].x), \
-        int(space_objects[0].y))), True, (0, 230, 230)), (0, 60))
 
 def step(): # шаг
     for i in range(len(space_objects)): # текущий
@@ -71,10 +62,10 @@ def step(): # шаг
 
             r = dx * dx + dy * dy # R^2
             if r < 0.1: 
-                r = 0.1 # избегаем деления на очень маленькое число
+                r = 0.1
             a = G * space_objects[j].m / r
             r = r ** 0.5 # R
-            if abs(dx) > space_objects[j].r or abs(dy) > space_objects[j].r:
+            if abs(dx) > space_objects[i].r + space_objects[j].r or abs(dy) > space_objects[i].r + space_objects[j].r:
                 ax = a * dx / r # a * cos
                 ay = a * dy / r # a * sin
                 space_objects[i].vx += ax * dt
@@ -102,14 +93,16 @@ if __name__ == '__main__':
     user.screen = pygame.display.set_mode(user.size, pygame.FULLSCREEN, pygame.OPENGL, vsync=1)
     pygame.display.set_caption('Space_physics')
     clock = pygame.time.Clock()
-    font = pygame.font.Font(None, 30)
+    font = pygame.font.Font('data/Anonymous Pro B.ttf', 25)
     G = 100
-    speed = 100
+    speed = 1
     dt = (1/user.fps) * speed # шаг времени для объектов
 
     space_objects = []
-    aStar = star(300, 300)
+    aStar = obj(300, 300, star_img, 16, 1) # x, y, img, r, m
     space_objects.append(aStar)
+    #aPlanet = obj(500, 500, planet_img, 35, 1000)
+    #space_objects.append(aPlanet)
 
     running = True
     while running:
@@ -128,15 +121,15 @@ if __name__ == '__main__':
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 4: # вперед
                     zoom += 0.1
-                    if movement_speed > 6:
-                        movement_speed -= 2
+                    if zoom > 4:
+                        zoom += 0.1
+                    zoom = round(zoom, 1)
                 elif event.button == 5: # назад
-                    zoom -= 0.1
-                    if zoom <= 0.1:
-                        zoom = 0.1
-                    movement_speed += 2
-                elif event.button == 1:
-                    aStar = star(int(event.pos[0] / zoom - movement[0] - 16), int(event.pos[1] / zoom - movement[1] - 16))
+                    if zoom > 0.1:
+                        zoom -= 0.1
+                        zoom = round(zoom, 1)
+                elif event.button == 1: # ЛКМ
+                    aStar = obj(int(event.pos[0] / zoom - movement[0]), int(event.pos[1] / zoom - movement[1]), star_img, 16, 1)
                     space_objects.append(aStar)
 
             if event.type == KEYUP:
@@ -149,20 +142,18 @@ if __name__ == '__main__':
                 if event.key == K_s:
                     moving_down = False
         if moving_up:
-            movement[1] += movement_speed
+            movement[1] += movement_speed / zoom
         if moving_down:
-            movement[1] -= movement_speed
+            movement[1] -= movement_speed / zoom
         if moving_left:
-            movement[0] += movement_speed
+            movement[0] += movement_speed / zoom
         if moving_right:
-            movement[0] -= movement_speed  
+            movement[0] -= movement_speed / zoom
         display = pygame.Surface((user.size[0], user.size[1]))
         display.fill((0, 0, 0))
         step()
-        for i in range(len(space_objects)):
-                display.blit(pygame.transform.scale(star_img, (int(star_img.get_width() * zoom), \
-                    int(star_img.get_height() * zoom))), (int((space_objects[i].x + movement[0]) * zoom - 16), \
-                    int((space_objects[i].y + movement[1]) * zoom - 16)))
+        for i in range(len(space_objects)): # отрисовка объектов
+            space_objects[i].draw(zoom)
 
         user.screen.blit(display, (0,0))
         fps_val()
